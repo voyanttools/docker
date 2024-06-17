a94375ce7b3ff042ab5106d2a7bae4d090a3cdd5
Voyant Tools <voyanttools@gmail.com>
2024-06-17 09:19:33 -0600

    initial commit

---
+++ dev/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+BASE=/voyant-data
+
+# move data files from the image folder to the mounted data folder
+#cp -nr /default-data/* $BASE
+
+# increase maximum post size
+sed -i "s%connectionTimeout=\"20000\"%maxPostSize=\"-1\" connectionTimeout=\"120000\"%g" $CATALINA_HOME/conf/server.xml
+# increase cache
+sed -i "s%<Context>%<Context>\n    <Resources cacheMaxSize=\"51200\" />%g" $CATALINA_HOME/conf/context.xml
+
+# enable entities
+sed -i 's%<param-name>entitiesenabled</param-name><param-value>false%<param-name>entitiesenabled</param-name><param-value>true%' \
+   /usr/local/tomcat/webapps/ROOT/WEB-INF/web.xml
+
+# continue execution
+# exec "$@"
+catalina.sh run
